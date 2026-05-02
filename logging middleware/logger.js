7acd47b6fe4c9a7c73d0c5ebe699561f792f6a1c
V Krishnakumar <drkrishnav06@gmail.com>
2026-05-02 11:30:56 +0530

    moved sensitive token to environment variables

--- logging middleware/logger.js
+++ logging middleware/logger.js
@@ -1,8 +1,8 @@
 const axios = require("axios");
 
 async function Log(level, pkg, message) {
-    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJrdjQ1NTNAc3JtaXN0LmVkdS5pbiIsImV4cCI6MTc3NzcwMDcyOCwiaWF0IjoxNzc3Njk5ODI4LCJpc3MiOiJBZmZvcmQgTWVkaWNhbCBUZWNobm9sb2dpZXMgUHJpdmF0ZSBMaW1pdGVkIiwianRpIjoiODYzZDM4OTAtMmI3OC00NzBmLTkxN2MtNjZkZGIyZDhiYjBhIiwibG9jYWxlIjoiZW4tSU4iLCJuYW1lIjoidiBrcmlzaG5ha3VtYXIiLCJzdWIiOiJhMTQwMTczNy01ZGI4LTQ4MzQtOTRiMy01ZmJhOTRhZTgyZjAifSwiZW1haWwiOiJrdjQ1NTNAc3JtaXN0LmVkdS5pbiIsIm5hbWUiOiJ2IGtyaXNobmFrdW1hciIsInJvbGxObyI6InJhMjMxMTAyNjAyMDA3NCIsImFjY2Vzc0NvZGUiOiJRa2JweEgiLCJjbGllbnRJRCI6ImExNDAxNzM3LTVkYjgtNDgzNC05NGIzLTVmYmE5NGFlODJmMCIsImNsaWVudFNlY3JldCI6InlLRVhKVlBQR0dNdnBxa3IifQ.Wr97GIBnc1gAnfx5lP1lQNAYeGORkXPxYb4HjdBIUIo";
-
+    require("dotenv").config();
+    const token = process.env.TOKEN;
     try {
         const response = await axios.post(
             "http://20.207.122.201/evaluation-service/logs",
